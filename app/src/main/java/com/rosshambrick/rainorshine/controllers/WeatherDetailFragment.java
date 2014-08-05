92853c5a1501150ed515bcc060dcd66e9747cf49
Ross Hambrick <ross@bignerdranch.com>
2014-08-05 17:26:02 -0400

    Made the fragment implement the Observer interface

--- app/src/main/java/com/rosshambrick/rainorshine/controllers/WeatherDetailFragment.java
+++ app/src/main/java/com/rosshambrick/rainorshine/controllers/WeatherDetailFragment.java
@@ -6,19 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rosshambrick.rainorshine.R;
 import com.rosshambrick.rainorshine.core.model.entities.CityWeather;
 import com.rosshambrick.rainorshine.core.model.services.WeatherRepo;
-import com.rosshambrick.rainorshine.core.networking.model.WeatherData;
 
 import javax.inject.Inject;
 
+import rx.Observer;
 import rx.Subscription;
 import rx.android.observables.AndroidObservable;
-import rx.functions.Action1;
 
-public class WeatherDetailFragment extends RainOrShineFragment {
+public class WeatherDetailFragment extends RainOrShineFragment implements Observer<CityWeather> {
 
     private static final String CITY_ID = "city_id";
 
@@ -45,26 +45,37 @@ public class WeatherDetailFragment extends RainOrShineFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_weather_detail, container, false);
-        mCityView = (TextView) view.findViewById(R.id.fragment_weather_detail_city);
+        mCityView = (TextView) view.findViewById(R.id.fragment_weather_detail_temperature);
         return view;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        mSubscription = AndroidObservable.bindFragment(this, mWeatherRepo.getCityById(mCityId))
-                .subscribe(new Action1<CityWeather>() {
-                    public void call(CityWeather weatherData) {
-                        display(weatherData);
-                    }
-                });
+        mSubscription = AndroidObservable
+                .bindFragment(this, mWeatherRepo.getCityById(mCityId))
+                .subscribe(this);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         mSubscription.unsubscribe();
+    }
+
+    @Override
+    public void onNext(CityWeather cityWeather) {
+        display(cityWeather);
+    }
+
+    @Override
+    public void onCompleted() {
+        //do nothing
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_LONG).show();
     }
 
     private void display(CityWeather weatherData) {
